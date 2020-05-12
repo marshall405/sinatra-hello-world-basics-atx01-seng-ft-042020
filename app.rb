2edb75f9a70b660116656d8c31075a3a82e33bf7
marshall405 <marshall.slemp@gmail.com>
2020-05-11 21:34:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do
+    if params['name']
+      "Hello, #{params['name']}!"
+    else
+      "Hello, World!"
+    end
+  end
 
 end
